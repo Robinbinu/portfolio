e4d108020ae4e810b0fce2f2798bf26f623361a7
Zodsenpai <91971225+Robinbinu@users.noreply.github.com>
2025-05-04 02:10:42 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,12 @@ import tailwindcss from "@tailwindcss/vite";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
-import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://robin.github.io',
   base: 'portfolio',
-  integrations: [mdx(), sitemap(), icon(),tailwind()],
+  integrations: [mdx(), sitemap(), icon()],
   vite: {
     plugins: [tailwindcss()],
   },
